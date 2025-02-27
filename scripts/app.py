cfce2eebfd5c7151443317ead4e5a1a4472823dc
Ashley Warden <ashwarden@outlook.com.au>
2025-02-27 22:58:49 +1100

    t4

--- scripts/app.py
+++ scripts/app.py
@@ -2,7 +2,7 @@
 # visit http://127.0.0.1:8050/ in your web browser.
 import csv
 
-from dash import Dash, html, dcc
+from dash import Dash, html, dcc, callback, Output, Input
 import plotly.express as px
 import pandas as pd
 
@@ -11,6 +11,8 @@ app = Dash()
 sales = []
 dates = []
 regions = []
+
+region_choices = ["north", "south", "east", "west", "all"]
 
 with open('../data/daily_sales_data_combined.csv', newline='') as csvfile:
     reader = csv.reader(csvfile, delimiter=',', quotechar='|')
@@ -27,16 +29,34 @@ df = pd.DataFrame({
     "Region": regions
 })
 
-fig = px.line(df, x="Date", y="Sales", color="Region")
-
 app.layout = html.Div(children=[
     html.H1(children='Pink Morsel sales over time'),
 
     dcc.Graph(
         id='example-graph',
-        figure=fig
-    )
+    ),
+
+    dcc.RadioItems(
+        list(region_choices),
+        'North',
+        id='region-radio',
+    ),
 ])
+
+@callback(
+    Output('example-graph', 'figure'),
+    Input('region-radio', 'value'))
+def update_figure(region_value):
+    if region_value == "all":
+        filtered_df = df
+    else:
+        filtered_df = df[df.Region == region_value]
+
+    fig = px.line(filtered_df, x="Date", y="Sales", color="Region")
+
+    fig.update_layout(transition_duration=500)
+
+    return fig
 
 if __name__ == '__main__':
     app.run(debug=True)
